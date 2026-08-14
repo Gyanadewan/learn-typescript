71227e5efc4f8b026d634fd558cc64c254c2af77
Gyanadewan <gyanadewan1@gmail.com>
2026-08-14 17:15:25 +0600

    first typescript code

---
+++ first.js
@@ -0,0 +1,7 @@
+"use strict";
+let user = "Gyana";
+let age = 18;
+let isStudent = true;
+console.log(user);
+console.log(age);
+console.log(isStudent);
---
+++ first.ts
@@ -0,0 +1,8 @@
+
+let user: string = "Gyana";
+let age: number = 18;
+let isStudent: boolean = true;
+
+console.log(user);
+console.log(age);
+console.log(isStudent)

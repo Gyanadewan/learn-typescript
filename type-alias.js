35b3b08e45aca4c409f83237ed58b58350f8dd18
Gyanadewan <gyanadewan1@gmail.com>
2026-08-18 23:38:29 +0600

    practicing type alias in typeScript

---
+++ type-alias.js
@@ -0,0 +1,6 @@
+"use strict";
+const user = {
+    name: "Gyana",
+    age: 18
+};
+console.log(user);
---
+++ type-alias.ts
@@ -0,0 +1,10 @@
+type User = {
+    name: string ;
+    age ? : number;
+}
+const user: User = {
+    name : "Gyana",
+    age : 18
+}
+
+console.log(user)
